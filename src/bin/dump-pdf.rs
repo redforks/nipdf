37da1ff5798d8335ae03260cb7a3da19090e280f
forks <redforks@gmail.com>
2023-05-31 14:33:42 +0800

    Load and dump pdf version

--- src/bin/dump-pdf.rs
+++ src/bin/dump-pdf.rs
@@ -1,6 +1,14 @@
 use std::env::args;
 
+use lopdf::Document;
+
+fn dump_ver(doc: &Document) {
+    println!("PDF version: {}", doc.version);
+}
+
 fn main() {
     let filename = args().nth(1).expect("Usage: dump-pdf <filename>");
-    println!("{}", filename);
+
+    let doc = Document::load(filename).unwrap();
+    dump_ver(&doc);
 }
