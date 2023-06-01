9013ccf135c5f908e366949ada7e41e819baaff3
forks <redforks@gmail.com>
2023-06-01 20:22:18 +0800

    Support sub-command through clap

--- src/bin/dump-pdf.rs
+++ src/bin/dump-pdf.rs
@@ -1,6 +1,6 @@
+use clap::{arg, Command};
 use pdf2docx::dump::object::DictionaryDumper;
 use pdf2docx::dump::xref::XrefDumper;
-use std::env::args;
 
 use lopdf::Document;
 
@@ -16,11 +16,29 @@ fn dump_trailer(doc: &Document) {
     println!("{:}", DictionaryDumper::new(&doc.trailer));
 }
 
-fn main() {
-    let filename = args().nth(1).expect("Usage: dump-pdf <filename>");
+fn cli() -> Command {
+    Command::new("dump-pdf")
+        .about("Dump PDF file structure and contents")
+        .subcommand_required(true)
+        .arg(arg!(<filename> "PDF file to dump"))
+        .subcommand(
+            Command::new("summary")
+                .visible_alias("ls")
+                .about("Dump PDF file summary"),
+        )
+}
 
+fn main() {
+    let matches = cli().get_matches();
+    let filename: &String = matches.get_one("filename").unwrap();
     let doc = Document::load(filename).unwrap();
-    dump_basic_info(&doc);
-    dump_trailer(&doc);
-    println!("\n{}", XrefDumper::new(&doc.reference_table));
+
+    match cli().get_matches().subcommand() {
+        Some(("summary", _)) => {
+            dump_basic_info(&doc);
+            dump_trailer(&doc);
+            println!("\n{}", XrefDumper::new(&doc.reference_table));
+        }
+        _ => todo!(),
+    }
 }
