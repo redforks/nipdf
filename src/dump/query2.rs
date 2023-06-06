199ba13c3b0ff23ddf7b6186d8c6397f13d77659
forks <redforks@gmail.com>
2023-06-06 16:40:21 +0800

    impl search_everywhere_matches()

--- src/dump/query2.rs
+++ src/dump/query2.rs
@@ -48,8 +48,67 @@ fn as_str(v: &Primitive) -> Cow<str> {
     }
 }
 
+struct Contains<'a> {
+    needle: Cow<'a, str>,
+    ignore_case: bool,
+}
+
+impl<'a> Contains<'a> {
+    fn new(needle: &'a str, ignore_case: bool) -> Self {
+        let needle = if ignore_case {
+            needle.to_ascii_lowercase().into()
+        } else {
+            needle.into()
+        };
+        Self {
+            needle,
+            ignore_case,
+        }
+    }
+
+    fn contains(&self, haystack: &str) -> bool {
+        if self.ignore_case {
+            haystack.to_ascii_lowercase().contains(self.needle.as_ref())
+        } else {
+            haystack.contains(self.needle.as_ref())
+        }
+    }
+}
+
+fn iter_self_and_children(v: &Primitive) -> impl Iterator<Item = &Primitive> {
+    let mut stack = vec![v];
+    std::iter::from_fn(move || {
+        if let Some(v) = stack.pop() {
+            match v {
+                Primitive::Array(a) => {
+                    stack.extend(a.iter());
+                }
+                Primitive::Dictionary(d) => {
+                    stack.extend(d.values());
+                }
+                Primitive::Stream(s) => {
+                    stack.extend(s.info.iter().map(|(_, v)| v));
+                }
+                _ => {}
+            }
+            Some(v)
+        } else {
+            None
+        }
+    })
+}
+
 fn search_everywhere_matches(v: &Primitive, s: &str, ignore_case: bool) -> bool {
-    todo!()
+    let contains = Contains::new(s, ignore_case);
+    fn inner(v: &Primitive, contains: &Contains) -> bool {
+        contains.contains(as_str(v).as_ref())
+            || match v {
+                Primitive::Dictionary(d) => d.iter().any(|(k, _)| contains.contains(k.as_str())),
+                _ => false,
+            }
+    }
+
+    iter_self_and_children(v).any(|v| inner(v, &contains))
 }
 
 fn search_name_only_matches(v: &Primitive, s: &str, ignore_case: bool) -> bool {
--- src/dump/query2/tests.rs
+++ src/dump/query2/tests.rs
@@ -1,5 +1,5 @@
 use super::*;
-use crate::object::{new_name, new_pdf_string, new_plain_ref};
+use crate::object::{new_dictionary1, new_pdf_string, new_plain_ref};
 use istring::small::SmallString;
 use pdf::primitive::{Dictionary, Name, Primitive};
 use test_case::test_case;
@@ -25,4 +25,25 @@ fn parse_field_query(exp: FieldQuery, s: &str) {
 #[test_case("name", Name(SmallString::from("name")); "name")]
 fn test_as_str(exp: &str, v: impl Into<Primitive>) {
     assert_eq!(as_str(&v.into()), Cow::from(exp));
+}
+
+#[test_case(true, "foo", "foo", false; "exact match")]
+#[test_case(true, "Foo", "fOo", true; "exact match, ignore case")]
+#[test_case(true, "foo", "foobar", false; "contains")]
+#[test_case(true, "Foo", "fOoBar", true; "contains, ignore case")]
+#[test_case(false, "foo", "bar", false; "no match")]
+#[test_case(false, "foobar", "foo", true; "no contains")]
+fn test_contains(exp: bool, needle: &str, haystack: &str, ignore_case: bool) {
+    let c = Contains::new(needle, ignore_case);
+    assert_eq!(c.contains(haystack), exp);
+}
+
+#[test_case(true, Primitive::Null, "null")]
+#[test_case(false, Primitive::Null, "blah")]
+#[test_case(true, new_dictionary1("foo", 1), "foo")]
+#[test_case(true, vec![Dictionary::new().into()], "DIC")]
+#[test_case(true, new_dictionary1("blah", new_pdf_string("foo")), "foo")]
+#[test_case(true, new_dictionary1("blah", vec![new_pdf_string("foo").into()]), "foo")]
+fn test_search_everywhere(exp: bool, v: impl Into<Primitive>, s: &str) {
+    assert_eq!(search_everywhere_matches(&v.into(), s, false), exp);
 }
