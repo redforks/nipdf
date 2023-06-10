7acdb4692c74d7cc3d9396f3a863a1a0e82babe8
forks <redforks@gmail.com>
2023-06-10 08:27:35 +0800

    File Tail struct

--- src/file.rs
+++ src/file.rs
@@ -6,4 +6,23 @@ impl<'a> Header<'a> {
     pub fn new(buf: &'a [u8]) -> Self {
         Self(buf)
     }
+
+    pub fn ver(&self) -> &str {
+        std::str::from_utf8(self.0).unwrap()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Tail {
+    xref_pos: u32,
+}
+
+impl Tail {
+    pub fn new(xref_pos: u32) -> Self {
+        Self { xref_pos }
+    }
+
+    pub fn xref_pos(&self) -> u32 {
+        self.xref_pos
+    }
 }
