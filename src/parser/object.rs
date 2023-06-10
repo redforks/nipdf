eaf467e24f0c3bb02bce43d528187db93f186303
forks <redforks@gmail.com>
2023-06-10 11:26:14 +0800

    Parses Object::Number

--- src/parser/object.rs
+++ src/parser/object.rs
@@ -1,4 +1,5 @@
-use nom::{branch::alt, bytes::complete::tag, character::complete::i32, combinator::map};
+use nom::{branch::alt, bytes::complete::tag, combinator::map, number::complete::float};
+use num::cast;
 
 use crate::object::Object;
 
@@ -8,8 +9,14 @@ pub fn parse_object(buf: &[u8]) -> ParseResult<'_, Object> {
     let null_parser = map(tag("null"), |_| Object::Null);
     let true_parser = map(tag("true"), |_| Object::Bool(true));
     let false_parser = map(tag("false"), |_| Object::Bool(false));
-    let i32_parser = map(i32, |v| Object::Integer(v));
-    alt((null_parser, true_parser, false_parser, i32_parser))(buf)
+    let f32_parser = map(float, |v| {
+        if let Some(i) = cast(v) {
+            Object::Integer(i)
+        } else {
+            Object::Number(v)
+        }
+    });
+    alt((null_parser, true_parser, false_parser, f32_parser))(buf)
 }
 
 #[cfg(test)]
