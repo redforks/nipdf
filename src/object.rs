9a3d52ba6e28987cab05b14f4307f05856e266e4
forks <redforks@gmail.com>
2023-06-10 11:05:55 +0800

    Impl From traits for Object

--- src/object.rs
+++ src/object.rs
@@ -2,18 +2,74 @@ use std::collections::HashMap;
 
 type Dictionary<'a> = HashMap<Name<'a>, Object<'a>>;
 type Array<'a> = Vec<Object<'a>>;
+type Stream<'a> = (Dictionary<'a>, &'a [u8]); // data part not including the stream/endstream keyword
 
+#[derive(Clone, PartialEq, Debug)]
 pub enum Object<'a> {
     Null,
-    Boolean(bool),
+    Bool(bool),
     Integer(i32),
     Number(f32),
     String(&'a [u8]),
     Name(Name<'a>), // without the leading slash
     Dictionary(Dictionary<'a>),
     Array(Array<'a>),
-    Stream((Dictionary<'a>, &'a [u8])), // data part not including the stream/endstream keyword
+    Stream(Stream<'a>),
     Reference(Reference),
+}
+
+impl<'a> From<Stream<'a>> for Object<'a> {
+    fn from(value: Stream<'a>) -> Self {
+        Self::Stream(value)
+    }
+}
+
+impl<'a> From<Array<'a>> for Object<'a> {
+    fn from(value: Array<'a>) -> Self {
+        Self::Array(value)
+    }
+}
+
+impl<'a> From<Reference> for Object<'a> {
+    fn from(value: Reference) -> Self {
+        Self::Reference(value)
+    }
+}
+
+impl<'a> From<Dictionary<'a>> for Object<'a> {
+    fn from(value: Dictionary<'a>) -> Self {
+        Self::Dictionary(value)
+    }
+}
+
+impl<'a> From<Name<'a>> for Object<'a> {
+    fn from(value: Name<'a>) -> Self {
+        Self::Name(value)
+    }
+}
+
+impl<'a> From<&'a [u8]> for Object<'a> {
+    fn from(value: &'a [u8]) -> Self {
+        Self::String(value)
+    }
+}
+
+impl<'a> From<f32> for Object<'a> {
+    fn from(value: f32) -> Self {
+        Self::Number(value)
+    }
+}
+
+impl<'a> From<i32> for Object<'a> {
+    fn from(value: i32) -> Self {
+        Self::Integer(value)
+    }
+}
+
+impl<'a> From<bool> for Object<'a> {
+    fn from(value: bool) -> Self {
+        Self::Bool(value)
+    }
 }
 
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
