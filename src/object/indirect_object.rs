ac39a442b0d5e446103d2830b6004703755d0b38
forks <redforks@gmail.com>
2023-06-14 17:49:18 +0800

    IndirectObject stores object instead of content
    Because IndirectObject may stored in Stream, it is
    impossible not to parsed content object to correctly find
    endobj position

--- src/object/indirect_object.rs
+++ src/object/indirect_object.rs
@@ -1,23 +1,17 @@
-use crate::parser::{parse_complete_object, ParseError};
-
 use super::Object;
-use once_cell::unsync::OnceCell;
-use std::result::Result;
 
 pub struct IndirectObject<'a> {
     id: u32,
     generation: u16,
-    content: &'a [u8],
-    resolved: OnceCell<Object<'a>>,
+    object: Object<'a>,
 }
 
 impl<'a> IndirectObject<'a> {
-    pub fn new(id: u32, generation: u16, content: &'a [u8]) -> Self {
+    pub fn new(id: u32, generation: u16, object: Object<'a>) -> Self {
         Self {
             id,
             generation,
-            content,
-            resolved: OnceCell::new(),
+            object,
         }
     }
 
@@ -29,11 +23,7 @@ impl<'a> IndirectObject<'a> {
         self.generation
     }
 
-    pub fn object(&self) -> Result<&Object<'a>, ParseError> {
-        self.resolved
-            .get_or_try_init(|| parse_complete_object(self.content))
+    pub fn object(&self) -> &Object<'a> {
+        &self.object
     }
 }
-
-#[cfg(test)]
-mod tests;
--- src/object/indirect_object/tests.rs
+++
@@ -1,7 +0,0 @@
-use super::*;
-
-#[test]
-fn resolve_object() {
-    let obj = IndirectObject::new(1, 0, b"123");
-    assert_eq!(obj.object().unwrap(), &Object::Integer(123));
-}
