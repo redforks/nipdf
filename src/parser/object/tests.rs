9f027d8dc297e3b40b47e94448633b4671e1c966
forks <redforks@gmail.com>
2023-06-15 21:25:53 +0800

    parse_dict() to snapshot test

--- src/parser/object/tests.rs
+++ src/parser/object/tests.rs
@@ -36,17 +36,12 @@ fn test_parse_array(exp: Vec<Object<'static>>, buf: impl AsRef<[u8]>) {
     assert_eq!((b"".as_slice(), exp), parse_array(buf.as_ref()).unwrap());
 }
 
-#[test_case(vec![], b"<< >>"; "empty dict")]
-#[test_case(vec![], b"<<>>"; "empty dict 2")]
-#[test_case(vec![(Name::new(b"/Type"), Name::new(b"/Catalog").into())], b"<< /Type /Catalog >>"; "dict with one entry")]
-#[test_case(
-    vec![(Name::new(b"/Inner"), Dictionary::new().into())],
-    b"<</Inner <<>>>>"; "nested")]
-fn test_parse_dict(exp: Vec<(Name, Object)>, buf: impl AsRef<[u8]>) {
-    assert_eq!(
-        parse_dict(buf.as_ref()),
-        Ok((&[][..], exp.into_iter().collect()))
-    );
+#[test_case(b"<< >>", "empty dict")]
+#[test_case(b"<<>>", "empty dict 2")]
+#[test_case(b"<< /Type /Catalog >>", "dict with one entry")]
+#[test_case(b"<</Inner <<>>>>", "nested")]
+fn test_parse_dict(buf: impl AsRef<[u8]>, name: &str) {
+    insta::assert_debug_snapshot!(name, parse_dict(buf.as_ref()).unwrap());
 }
 
 #[test_case(
