c2fb5b8ef0ac922704adf10e228a53924a0cc99c
forks <redforks@gmail.com>
2023-06-16 09:19:10 +0800

    Sort sample files, parse_objects_test skip bizarre files

--- tests/parse_objects_test.rs
+++ tests/parse_objects_test.rs
@@ -3,7 +3,7 @@ use pdf2docx::parser::parse_frame_set;
 
 #[test]
 fn scan_objects() {
-    for entry in glob("doc/*.pdf").unwrap() {
+    for entry in glob("sample_files/normal/**/*.pdf").unwrap() {
         let path = entry.unwrap();
         let buf = std::fs::read(&path).unwrap();
         let (_, _frames) = parse_frame_set(&buf)
