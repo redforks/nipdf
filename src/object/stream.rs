c7cf7d763f9d870021b32d05c36c3e426c80667a
forks <redforks@gmail.com>
2023-06-18 15:42:47 +0800

    Touch Stream::decode()

--- src/object/stream.rs
+++ src/object/stream.rs
@@ -1,4 +1,13 @@
-use super::Dictionary;
+use std::borrow::Cow;
+
+use super::{Dictionary, ObjectValueError};
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct Stream<'a>(pub Dictionary<'a>, pub &'a [u8]);
+
+impl<'a> Stream<'a> {
+    /// Decode stream data using filter and parameters in stream dictionary.
+    pub fn decode(&self) -> Result<Cow<[u8]>, ObjectValueError> {
+        todo!()
+    }
+}
