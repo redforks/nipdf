198a24ebb4b7e61a4f475531d80119cc6571787b
forks <redforks@gmail.com>
2023-06-20 15:00:13 +0800

    dump-stream sub command

--- src/bin/dump-pdf.rs
+++ src/bin/dump-pdf.rs
@@ -1,24 +1,62 @@
-use std::process::ExitCode;
+use std::{
+    borrow::Borrow,
+    io::{copy, stdout, Stdout},
+};
+
+use anyhow::Result as AnyResult;
 
 use clap::{arg, Command};
+use pdf2docx::{file::File, object::Object};
 
 fn cli() -> Command {
     Command::new("dump-pdf")
         .about("Dump PDF file structure and contents")
         .subcommand_required(true)
-        // .arg(arg!(<filename> "PDF file to dump"))
-        .subcommand(Command::new("hello").visible_alias("ls").about("Hello"))
+        .subcommand(
+            Command::new("dump-stream")
+                .about("dump stream content to stdout")
+                .arg(arg!(-f <filename> "PDF file to dump"))
+                .arg(arg!(<object_id> "object ID to dump"))
+                .arg(arg!(--raw "Skip decoding stream content")),
+        )
 }
 
-fn hello() {
-    println!("Hello, world!");
+fn dump_stream(path: &str, id: u32, raw: bool) -> AnyResult<()> {
+    let buf = std::fs::read(path).unwrap();
+    let (_f, mut resolver) =
+        File::parse(&buf[..]).unwrap_or_else(|_| panic!("failed to parse {:?}", path));
+    let obj = resolver.resolve(id);
+    match obj {
+        None => eprintln!("object id not found"),
+        Some(obj) => {
+            match obj {
+                Object::Stream(s) => {
+                    let decoded;
+                    let mut buf = if raw {
+                        s.1
+                    } else {
+                        decoded = s.decode()?;
+                        decoded.borrow()
+                    };
+                    copy(&mut buf, &mut stdout())?;
+                }
+                _ => eprintln!("object is not a stream"),
+            };
+        }
+    }
+    Ok(())
 }
 
-fn main() -> ExitCode {
+fn main() -> AnyResult<()> {
     match cli().get_matches().subcommand() {
-        Some(("hello", _)) => hello(),
+        Some(("dump-stream", sub_m)) => dump_stream(
+            sub_m.get_one::<String>("filename").unwrap(),
+            sub_m
+                .get_one::<String>("object_id")
+                .and_then(|s| s.parse().ok())
+                .unwrap(),
+            sub_m.get_one::<bool>("raw").copied().unwrap_or_default(),
+        ),
         _ => todo!(),
     }
-
-    ExitCode::SUCCESS
 }
