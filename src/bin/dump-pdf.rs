a6a657ed66315a18576e0e0f2d27b534725ccb57
forks <redforks@gmail.com>
2023-07-06 22:57:30 +0800

    --png option to stream subcommand of dump-pdf

--- src/bin/dump-pdf.rs
+++ src/bin/dump-pdf.rs
@@ -1,11 +1,12 @@
 use std::{
     borrow::Borrow,
-    io::{copy, stdout},
+    io::{copy, stdout, Cursor},
 };
 
 use anyhow::Result as AnyResult;
 
 use clap::{arg, Command};
+use image::ImageOutputFormat;
 use pdf2docx::{file::File, object::Object};
 
 fn cli() -> Command {
@@ -18,11 +19,13 @@ fn cli() -> Command {
                 .arg(arg!(-f <filename> "PDF file to dump"))
                 .arg(arg!(<object_id> "object ID to dump"))
                 .arg(arg!(--raw "Skip decoding stream content"))
-                .arg(arg!(--image "Assume stream is image, convert to JPEG or PNG based on stream type")),
+                .arg(arg!(--image "Assume stream is image, convert to JPEG or PNG based on stream type"))
+                .arg(arg!(--png "Assume stream is image, decode and convert to PNG"))
+                ,
         )
 }
 
-fn dump_stream(path: &str, id: u32, raw: bool, as_image: bool) -> AnyResult<()> {
+fn dump_stream(path: &str, id: u32, raw: bool, as_image: bool, as_png: bool) -> AnyResult<()> {
     let buf = std::fs::read(path).unwrap();
     let (_f, mut resolver) =
         File::parse(&buf[..]).unwrap_or_else(|_| panic!("failed to parse {:?}", path));
@@ -31,11 +34,18 @@ fn dump_stream(path: &str, id: u32, raw: bool, as_image: bool) -> AnyResult<()> 
         Object::Stream(s) => {
             let decoded;
             let image;
+            let dyn_image_buf;
             let mut buf = if as_image {
                 image = s.to_raw_image()?;
                 &image.data[..]
             } else if raw {
                 s.1
+            } else if as_png {
+                let img = s.to_dynamic_image()?;
+                let mut cursor = Cursor::new(Vec::new());
+                img.write_to(&mut cursor, ImageOutputFormat::Png)?;
+                dyn_image_buf = cursor.into_inner();
+                &dyn_image_buf[..]
             } else {
                 decoded = s.decode()?;
                 decoded.borrow()
@@ -59,6 +69,7 @@ fn main() -> AnyResult<()> {
                 .unwrap(),
             sub_m.get_one::<bool>("raw").copied().unwrap_or_default(),
             sub_m.get_one::<bool>("image").copied().unwrap_or_default(),
+            sub_m.get_one::<bool>("png").copied().unwrap_or_default(),
         ),
         _ => todo!(),
     }
