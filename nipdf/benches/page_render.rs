096a9febecffc4973da8849314a78d1b26093102
forks <redforks@gmail.com>
2023-11-06 19:11:09 +0800

    Add render_page criterion bench

---
+++ nipdf/benches/page_render.rs
@@ -0,0 +1,39 @@
+use std::time::Duration;
+
+use anyhow::Result as AnyResult;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use nipdf::file::{File, RenderOptionBuilder};
+
+fn read_sample_file(file_path: impl AsRef<std::path::Path>) -> Vec<u8> {
+    use std::path::Path;
+
+    let file_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("sample_files")
+        .join(file_path);
+    std::fs::read(file_path).unwrap()
+}
+
+/// Render specific page of pdf file.
+/// `file_path` relative to '~/sample_files/'.
+fn render_page(file_path: impl AsRef<std::path::Path>, no: usize) -> AnyResult<tiny_skia::Pixmap> {
+    let buf = read_sample_file(file_path);
+    let f = File::parse(buf)?;
+    let resolver = f.resolver()?;
+    let pages = f.catalog(&resolver)?.pages()?;
+    let page = pages.get(no).unwrap();
+    let option = RenderOptionBuilder::new().zoom(1.5);
+    Ok(page.render(option)?)
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("page render", |b| {
+        b.iter(|| render_page("../../../../ICEpower125ASX2_Datasheet_2.0.pdf", 1).unwrap())
+    });
+}
+
+criterion_group! {
+  name = benches;
+  config = Criterion::default().measurement_time(Duration::from_secs(20));
+  targets = criterion_benchmark
+}
+criterion_main!(benches);
