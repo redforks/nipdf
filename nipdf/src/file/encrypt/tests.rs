1531be9fa41d235fe5448822111409136930b52e
forks <redforks@gmail.com>
2023-11-11 10:56:19 +0800

    Add authorize_user test for revion2

--- nipdf/src/file/encrypt/tests.rs
+++ nipdf/src/file/encrypt/tests.rs
@@ -18,20 +18,28 @@ fn test_pad_trunc_password() {
 }
 
 #[test]
-fn test_authorize_user() {
-    // test case from pdfReferenceUpdated.pdf
-    let owner_hash = hex!("63981688733872DEC7983D3C6EB1F412CC535EA2DAA2AB171E2BBC4E36B21887");
-    let user_hash = hex!("D64AB15C7434FFE1732E6388274F64C428BF4E5E4E758A4164004E56FFFA0108");
-    let doc_id = hex!("9597C618BC90AFA4A078CA72B2DD061C");
+fn test_authorize_user_v2() {
+    // test case from 5177.Type2.pdf
+    let owner_hash = hex!("02d8a74390d98f0ca54f6e9fb9e607ced6f67f8c436a748d9add5bb883e5795d");
+    let user_hash = hex!("3dd77b55ad61a0f5d7bce45989272a151797e20b8333013f8f9b94c7e95bd68c");
+    let doc_id = hex!("9f564085cf0a9f7c1145bf1163e68212");
     assert!(authorize_user(
-        StandardHandlerRevion::V3,
+        StandardHandlerRevion::V2,
         40,
         b"",
         &owner_hash,
         &user_hash,
-        0xFFFF_FFE4,
+        unsafe { std::mem::transmute(-12i32) },
         &doc_id,
     ));
+}
+
+#[test]
+fn test_authorize_user_v3() {
+    // test case from pdfReferenceUpdated.pdf
+    let owner_hash = hex!("63981688733872DEC7983D3C6EB1F412CC535EA2DAA2AB171E2BBC4E36B21887");
+    let user_hash = hex!("D64AB15C7434FFE1732E6388274F64C428BF4E5E4E758A4164004E56FFFA0108");
+    let doc_id = hex!("9597C618BC90AFA4A078CA72B2DD061C");
     assert!(authorize_user(
         StandardHandlerRevion::V3,
         40,
