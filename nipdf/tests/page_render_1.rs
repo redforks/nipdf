d28e19873b2a131e3ef61a900dea4fdf984b3acf
forks <redforks@gmail.com>
2023-11-17 08:24:06 +0800

    Impl render test

--- nipdf/tests/page_render_1.rs
+++ nipdf/tests/page_render_1.rs
@@ -1,11 +1,15 @@
 //! Test page render result using `insta` to ensure that the rendering result is not changed.
 //! This file checks file pdfReferenceUpdated.pdf
 use anyhow::Result as AnyResult;
+use hex::ToHex;
 use insta::assert_ron_snapshot;
 use md5::{Digest, Md5};
-use nipdf::file::File;
+use nipdf::file::{File, RenderOptionBuilder};
 use nipdf_test_macro::file_render_test;
-use std::num::NonZeroU32;
+use std::{
+    num::NonZeroU32,
+    path::{Path, PathBuf},
+};
 use test_case::test_case;
 
 /// Decode pdf embed image and return the result as Vec<u8>.
@@ -29,7 +33,25 @@ fn image_separation_color_space() {
     assert_ron_snapshot!(&decode_image(1297).unwrap());
 }
 
+/// Read pdf file and render each page, to save test time,
+/// touch a file at `$CARGO_TARGET_TMPDIR/(md5(f))` if succeed.
+/// If the file exist, skips the test
 #[file_render_test]
-fn render(f: &str) {
-    dbg!(f);
+fn render(f: &str) -> AnyResult<()> {
+    let hash_file: String = Md5::digest(f.as_bytes()).as_slice().encode_hex();
+    let hash_file = Path::join(Path::new(env!["CARGO_TARGET_TMPDIR"]), hash_file);
+    if hash_file.exists() {
+        return Ok(());
+    }
+
+    let path = PathBuf::from(f);
+    let buf = std::fs::read(&path).unwrap();
+    let f = File::parse(buf, "", "").unwrap_or_else(|_| panic!("failed to parse {path:?}"));
+    let resolver = f.resolver().unwrap();
+    let catalog = f.catalog(&resolver)?;
+    for page in catalog.pages()? {
+        let option = RenderOptionBuilder::new().zoom(0.75);
+        page.render(option)?;
+    }
+    Ok(std::fs::write(&hash_file, "")?)
 }
