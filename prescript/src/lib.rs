df25326e1c26f47654d4e9aa7183814b9dded7cf
forks <redforks@gmail.com>
2023-11-18 15:52:03 +0800

    NameRegistry and Name

--- prescript/src/lib.rs
+++ prescript/src/lib.rs
@@ -4,7 +4,10 @@ pub(crate) mod parser;
 mod encoding;
 mod type1;
 pub use encoding::Encoding256;
+use string_interner::{backend::BucketBackend, symbol::SymbolU16, StringInterner};
 pub use type1::Font;
 
 /// Symbol for .notdef glyph
 pub const NOTDEF: &str = ".notdef";
+pub type Name = SymbolU16;
+pub type NameRegistry = StringInterner<BucketBackend<Name>>;
