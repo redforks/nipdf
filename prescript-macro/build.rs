6c05c245fbec66293271b44a9a8e31f2b5abdc2f
forks <redforks@gmail.com>
2023-11-22 14:24:55 +0800

    Skip update built_in_names.rs if names files not newer

--- prescript-macro/build.rs
+++ prescript-macro/build.rs
@@ -8,8 +8,22 @@
 use std::{fs::read_to_string, io::Write, path::Path};
 
 fn main() {
-    let p = Path::join(Path::new(env!("CARGO_MANIFEST_DIR")), "../names");
-    let names = read_to_string(p).unwrap();
+    let names_file = Path::join(Path::new(env!("CARGO_MANIFEST_DIR")), "../names");
+    let dest_file = Path::join(
+        Path::new(env!("CARGO_MANIFEST_DIR")),
+        "src/built_in_names.rs",
+    );
+
+    // return if names_file is not newer than dest_file
+    if let Ok(names_meta) = std::fs::metadata(&names_file) {
+        if let Ok(dest_meta) = std::fs::metadata(&dest_file) {
+            if names_meta.modified().unwrap() <= dest_meta.modified().unwrap() {
+                return;
+            }
+        }
+    }
+
+    let names = read_to_string(names_file).unwrap();
     let mut names: Vec<_> = names
         .lines()
         .map(|s| s.trim())
@@ -17,11 +31,7 @@ fn main() {
         .collect();
     names.sort_unstable();
     names.dedup();
-    let p = Path::join(
-        Path::new(env!("CARGO_MANIFEST_DIR")),
-        "src/built_in_names.rs",
-    );
-    let mut file = std::fs::File::create(p).unwrap();
+    let mut file = std::fs::File::create(dest_file).unwrap();
     let n = names.len();
     writeln!(file, "pub(crate) static BUILT_IN_NAMES: [&str; {n}] = [").unwrap();
     for name in names {
