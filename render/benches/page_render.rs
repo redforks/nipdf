34376e43f09722a53eb71edff0d29d899c039ce2
forks <redforks@gmail.com>
2023-12-09 21:42:28 +0800

    Move bench to render crate

--- render/benches/page_render.rs
+++ render/benches/page_render.rs
@@ -2,7 +2,8 @@ use anyhow::Result as AnyResult;
 use criterion::{criterion_group, criterion_main, Criterion};
 use image::RgbaImage;
 use mimalloc::MiMalloc;
-use nipdf::file::{File, RenderOptionBuilder};
+use nipdf::file::File;
+use nipdf_render::{render_page, RenderOptionBuilder};
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
@@ -10,33 +11,31 @@ static GLOBAL: MiMalloc = MiMalloc;
 fn read_sample_file(file_path: impl AsRef<std::path::Path>) -> Vec<u8> {
     use std::path::Path;
 
-    let file_path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("sample_files")
-        .join(file_path);
+    let file_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(file_path);
     std::fs::read(file_path).unwrap()
 }
 
 /// Render specific page of pdf file.
 /// `file_path` relative to '~/sample_files/'.
-fn render_page(file_path: impl AsRef<std::path::Path>, no: usize) -> AnyResult<RgbaImage> {
+fn render_page_no(file_path: impl AsRef<std::path::Path>, no: usize) -> AnyResult<RgbaImage> {
     let buf = read_sample_file(file_path);
     let f = File::parse(buf, "", "")?;
     let resolver = f.resolver()?;
     let pages = f.catalog(&resolver)?.pages()?;
     let page = &pages[no];
     let option = RenderOptionBuilder::new().zoom(1.5);
-    Ok(page.render(option)?)
+    Ok(render_page(page, option)?)
 }
 
 pub fn render1(c: &mut Criterion) {
     c.bench_function("page render", |b| {
-        b.iter(|| render_page("../../../pdf/ICEpower125ASX2_Datasheet_2.0.pdf", 1).unwrap())
+        b.iter(|| render_page_no("../../pdf/ICEpower125ASX2_Datasheet_2.0.pdf", 1).unwrap())
     });
 }
 
 pub fn render2(c: &mut Criterion) {
     c.bench_function("page render", |b| {
-        b.iter(|| render_page("../../../pdf/compressed.tracemonkey-pldi-09.pdf", 0).unwrap())
+        b.iter(|| render_page_no("../../pdf/compressed.tracemonkey-pldi-09.pdf", 0).unwrap())
     });
 }
 
