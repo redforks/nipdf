02e8a839d9bc8d08a291abc584a727267f33fd27
forks <redforks@gmail.com>
2023-12-21 22:02:32 +0800

    CodeSpace::next_code()

--- prescript/src/cmap.rs
+++ prescript/src/cmap.rs
@@ -1,16 +1,43 @@
 //! Cmap to map CharCode to CID, used in Type0/CID font
 
+use either::Either;
+use tinyvec::ArrayVec;
+
 /// Convert from CharCode using cmap, use it to select glyph id
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct CID(u16);
 
-/// Input code type, can be one/two/three bytes.
+/// Input code type, can be one/two/three/four bytes.
 /// TODO: bytes in any length
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum CharCode {
     One(u8),
     Two(u8, u8),
     Three(u8, u8, u8),
+    Four(u8, u8, u8, u8),
+}
+
+impl CharCode {
+    pub fn n_bytes(&self) -> usize {
+        match self {
+            Self::One(_) => 1,
+            Self::Two(_, _) => 2,
+            Self::Three(_, _, _) => 3,
+            Self::Four(_, _, _, _) => 4,
+        }
+    }
+}
+
+impl From<&[u8]> for CharCode {
+    fn from(bytes: &[u8]) -> Self {
+        match bytes.len() {
+            1 => Self::One(bytes[0]),
+            2 => Self::Two(bytes[0], bytes[1]),
+            3 => Self::Three(bytes[0], bytes[1], bytes[2]),
+            4 => Self::Four(bytes[0], bytes[1], bytes[2], bytes[3]),
+            _ => panic!("invalid bytes length"),
+        }
+    }
 }
 
 /// Common trait that maps CharCode to CID.
@@ -18,17 +45,85 @@ trait CodeMap {
     fn map(&self, code: CharCode) -> Option<CID>;
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+struct ByteRange {
+    lower: u8,
+    upper: u8,
+}
+
+impl ByteRange {
+    pub fn new(lower: u8, upper: u8) -> Self {
+        assert!(lower <= upper);
+        Self { lower, upper }
+    }
+
+    fn in_range(&self, c: u8) -> bool {
+        self.lower <= c && c <= self.upper
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum CodeSpaceResult {
+    /// Matched
+    Matched(CharCode),
+    /// Partial match, not enough bytes, or prefix bytes matched.
+    Partial(CharCode),
+    /// No match
+    NotMatched,
+}
+
 /// A range entry in code space, lower and upper must have the same length.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct CodeRange(CharCode, CharCode);
+/// A range matches N bytes, N is the length of inner array, each item
+/// defines a range of bytes, first item for first byte, second item for
+/// second byte, and so on.
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct CodeRange(ArrayVec<[ByteRange; 4]>);
+
+impl CodeRange {
+    /// Parse a range from string, for example:
+    ///
+    /// `parse("00", "08")`, returns a range that matches 1 byte, from 0x00 to 0x08.
+    /// `parse("0000", "0800")`, returns a range that matches 2 bytes, from 0x0000 to 0x0800.
+    fn parse(s_lower: &str, s_upper: &str) -> Option<Self> {
+        let lower = u32::from_str_radix(s_lower, 16).ok()?;
+        let upper = u32::from_str_radix(s_upper, 16).ok()?;
+        let n_bytes = s_lower.len() / 2;
+        assert_eq!(n_bytes, s_upper.len() / 2);
+        let mut r = ArrayVec::new();
+        for i in (0..n_bytes).into_iter().rev() {
+            let lower = (lower >> (i * 8)) as u8;
+            let upper = (upper >> (i * 8)) as u8;
+            r.push(ByteRange::new( lower, upper ));
+        }
+        Some(Self(r))
+    }
+
+    /// Find next code.
+    fn next_code(&self, codes: &[u8]) -> CodeSpaceResult {
+        match self
+            .0
+            .iter()
+            .zip(codes.iter().copied())
+            .take_while(|(r, c)| r.in_range(*c))
+            .count()
+        {
+            0 => CodeSpaceResult::NotMatched,
+            n if n == self.n_bytes() => CodeSpaceResult::Matched(CharCode::from(&codes[..n])),
+            _ => CodeSpaceResult::Partial(CharCode::from(&codes[..self.n_bytes().min(codes.len())])),
+        }
+    }
+
+    fn n_bytes(&self) -> usize {
+        self.0.len()
+    }
+}
 
 /// CodeSpace made up by CodeRanges.
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct CodeSpace(Box<[CodeRange]>);
 
 impl CodeSpace {
-    fn new(mut ranges: Vec<CodeRange>) -> Self {
-        ranges.sort();
+    fn new(ranges: Vec<CodeRange>) -> Self {
         Self(ranges.into_boxed_slice())
     }
 
@@ -36,7 +131,19 @@ impl CodeSpace {
     /// If next code not in code space, return `Err(next_code)`.
     /// Panic if input codes is empty.
     fn next_code<'a>(&self, codes: &'a [u8]) -> (&'a [u8], Result<CharCode, CharCode>) {
-        todo!()
+        let next = self
+            .0
+            .iter()
+            .find_map(|r| {
+                let r = r.next_code(codes);
+                match r {
+                    CodeSpaceResult::Matched(code) => Some(Either::Right(code)),
+                    CodeSpaceResult::Partial(code) => Some(Either::Left(code)),
+                    CodeSpaceResult::NotMatched => None,
+                }
+            })
+            .unwrap_or_else(|| Either::Left(CharCode::One(codes[0])));
+        (&codes[next.clone().into_inner().n_bytes()..], next.into())
     }
 }
 
--- prescript/src/cmap/tests.rs
+++ prescript/src/cmap/tests.rs
@@ -1,12 +1,109 @@
 use super::*;
+use tinyvec::array_vec;
+use CharCode::*;
+
+fn one(v: u8) -> CharCode {
+    One(v)
+}
+
+fn two(v: u16) -> CharCode {
+    Two((v >> 8) as u8, v as u8)
+}
+
+fn three(v: u32) -> CharCode {
+    Three((v >> 16) as u8, (v >> 8) as u8, v as u8)
+}
+
+fn four(v: u32) -> CharCode {
+    Four((v >> 24) as u8, (v >> 16) as u8, (v >> 8) as u8, v as u8)
+}
 
 #[test]
-fn compare_char_code() {
-    assert!(CharCode::One(100) < CharCode::Two(0, 0));
-    assert!(CharCode::Two(100, 200) < CharCode::Three(0, 0, 0));
-    assert!(CharCode::One(10) < CharCode::One(100));
-    assert!(CharCode::Two(10, 10) < CharCode::Two(10, 100));
-    assert!(CharCode::Two(10, 100) < CharCode::Two(11, 0));
-    assert!(CharCode::Three(10, 10, 10) < CharCode::Three(10, 10, 100));
-    assert!(CharCode::Three(10, 10, 10) < CharCode::Three(11, 0, 0));
+fn code_range_parse() {
+    assert_eq!(
+        CodeRange::parse("20", "7e").unwrap(),
+        CodeRange(array_vec!([ByteRange; 4] => ByteRange::new(0x20, 0x7e)))
+    );
+    assert_eq!(
+        CodeRange::parse("8140", "817e").unwrap(),
+        CodeRange(array_vec!([ByteRange; 4] =>
+            ByteRange::new(0x81, 0x81),
+            ByteRange::new(0x40, 0x7e)
+        ))
+    );
+    assert_eq!(
+        CodeRange::parse("D800DC00", "DBFFDFFF").unwrap(),
+        CodeRange(array_vec!([ByteRange; 4] =>
+            ByteRange::new(0xD8, 0xDB),
+            ByteRange::new(0x00, 0xFF),
+            ByteRange::new(0xDC, 0xDF),
+            ByteRange::new(0x00, 0xFF),
+        )),
+    );
+}
+
+#[test]
+fn code_range_next_code() {
+    // one
+    let r = CodeRange::parse("20", "7e").unwrap();
+    assert_eq!(CodeSpaceResult::Matched(one(0x20)), r.next_code(&[0x20, 0]));
+    assert_eq!(CodeSpaceResult::Matched(one(0x7e)), r.next_code(&[0x7e]));
+    assert_eq!(CodeSpaceResult::Matched(one(0x21)), r.next_code(&[0x21]));
+    assert_eq!(CodeSpaceResult::NotMatched, r.next_code(&[0x1f]));
+    assert_eq!(CodeSpaceResult::NotMatched, r.next_code(&[0x7f]));
+
+    // two
+    let r = CodeRange::parse("8140", "817e").unwrap();
+    assert_eq!(
+        CodeSpaceResult::Matched(two(0x8140)),
+        r.next_code(&[0x81, 0x40])
+    );
+    assert_eq!(
+        CodeSpaceResult::Matched(two(0x817e)),
+        r.next_code(&[0x81, 0x7e])
+    );
+    assert_eq!(
+        CodeSpaceResult::Matched(two(0x8141)),
+        r.next_code(&[0x81, 0x41])
+    );
+    assert_eq!(CodeSpaceResult::NotMatched, r.next_code(&[0x80, 0x40]));
+    assert_eq!(
+        CodeSpaceResult::Partial(two(0x817f)),
+        r.next_code(&[0x81, 0x7f])
+    );
+    assert_eq!(CodeSpaceResult::Partial(one(0x81)), r.next_code(&[0x81]));
+}
+
+#[test]
+fn code_space() {
+    let code_space = CodeSpace::new(vec![
+        CodeRange::parse("20", "7e").unwrap(),
+        CodeRange::parse("8140", "817e").unwrap(),
+        CodeRange::parse("D800DC00", "DBFFDFFF").unwrap(),
+        CodeRange::parse("E000", "FFFF").unwrap(),
+    ]);
+
+    // matches
+    assert_eq!(
+        (&[0u8][..], Ok(one(0x20))),
+        code_space.next_code(&[0x20, 0])
+    );
+
+    // not match
+    assert_eq!(
+        (&[0u8][..], Err(one(0x1f))),
+        code_space.next_code(&[0x1f, 0])
+    );
+
+    // four bytes matched
+    assert_eq!(
+        (&[][..], Ok(four(0xD800DC00))),
+        code_space.next_code(&[0xD8, 0x00, 0xDC, 0x00])
+    );
+
+    // two bytes partial matched
+    assert_eq!(
+        (&[][..], Err(two(0x817f))),
+        code_space.next_code(&[0x81, 0x7f]),
+    );
 }
