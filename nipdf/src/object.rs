c2d65a7efa73af2a118fedb175ba9c78e18fdb94
forks <redforks@gmail.com>
2024-11-30 21:58:47 +0800

    Hide IndirectObjectDef

--- nipdf/src/object.rs
+++ nipdf/src/object.rs
@@ -21,19 +21,11 @@ pub type Array = Rc<[Object]>;
 use snafu::{OptionExt, ResultExt, Snafu};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct IndirectObjectDef(pub(crate) ObjectId, pub(crate) Object);
+pub(crate) struct IndirectObjectDef(pub(crate) ObjectId, pub(crate) Object);
 
 impl IndirectObjectDef {
-    pub fn new(id: u32, generation: u16, object: Object) -> Self {
-        Self(ObjectId::new(id, generation), object)
-    }
-
     pub fn id(&self) -> ObjectId {
         self.0
-    }
-
-    pub fn object(&self) -> &Object {
-        &self.1
     }
 
     pub fn take(self) -> Object {
