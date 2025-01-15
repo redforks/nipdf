b403903831ff7ce3caf07a80a45cbbb523cad990
forks <redforks@gmail.com>
2025-01-15 15:20:20 +0800

    scan_objects() use snafu report

--- nipdf/tests/parse_objects_test.rs
+++ nipdf/tests/parse_objects_test.rs
@@ -1,29 +1,37 @@
 use glob::glob;
 use nipdf::{
+    Result,
     file::File,
     object::{Object, ObjectValueError, RuntimeObjectId},
 };
+use snafu::{FromString as _, ResultExt as _, Whatever, report};
 
+#[report]
 #[test_log::test]
-fn scan_objects() {
-    for entry in glob("sample_files/normal/**/*.pdf").unwrap() {
-        let path = entry.unwrap();
-        let buf = std::fs::read(&path).unwrap();
+fn scan_objects() -> Result<(), Whatever> {
+    for entry in glob("sample_files/normal/**/*.pdf").whatever_context("glob sample files")? {
+        let path = entry.whatever_context("get file entry")?;
+        let buf = std::fs::read(&path).whatever_context("read file")?;
         println!("parsing {path:?}");
-        let f = File::parse(buf, "").unwrap_or_else(|_| panic!("failed to parse {path:?}"));
-        let resolver = f.resolver().unwrap();
+        let f = File::parse(buf, "").whatever_context("open pdf file")?;
+        let resolver = f.resolver().whatever_context("get resolver")?;
         for id in 1..resolver.n() {
             print!("scan object: {id}");
             match resolver.resolve(RuntimeObjectId(id.try_into().unwrap())) {
                 Err(ObjectValueError::ObjectIDNotFound { .. }) => {
                     print!(" not found");
                 }
-                Err(e) => panic!("{}", e),
+                Err(e) => {
+                    return Err(Whatever::with_source(
+                        Box::new(e),
+                        "resolve object".to_owned(),
+                    ));
+                }
                 Ok(Object::Stream(s)) => s
                     .decode(&resolver)
                     .map(|_| ())
                     .or_else(|_| s.decode_image(&resolver, None).map(|_| ()))
-                    .unwrap(),
+                    .whatever_context("decode stream")?,
                 _ => {}
             }
 
@@ -32,9 +40,9 @@ fn scan_objects() {
 
         for (idx, page) in f
             .catalog(&resolver)
-            .unwrap()
+            .whatever_context("get catalog")?
             .pages()
-            .unwrap()
+            .whatever_context("get pages")?
             .iter()
             .enumerate()
         {
@@ -42,10 +50,16 @@ fn scan_objects() {
             println!("  media_box: {:?}", page.media_box());
             println!("  crop_box: {:?}", page.crop_box());
 
-            for op in page.content().unwrap().operations().unwrap() {
+            for op in page
+                .content()
+                .whatever_context("get page content")?
+                .operations()
+                .whatever_context("parse page operations")?
+            {
                 println!("  {:?}", op);
             }
         }
         println!();
     }
+    Ok(())
 }
