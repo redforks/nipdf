adc2d97cfc9649d76c3ef8dbf567c58dffcd7c5b
forks <redforks@gmail.com>
2025-02-24 21:15:45 +0800

    Ignore a page_render_1

--- render/tests/page_render_1.rs
+++ render/tests/page_render_1.rs
@@ -67,7 +67,7 @@ fn download_file(url: &str, p: impl AsRef<Path>) -> Result<()> {
 }
 
 /// These files are very rare and odd, not to be tested
-const IGNORED: [&str; 12] = [
+const IGNORED: [&str; 13] = [
     // odd FlateDecode stream, xpdf failed to decode, mupdf no problem
     "bug1050040.pdf",
     // invalid object format, mupdf failed to parse, mupdf says no page
@@ -110,6 +110,8 @@ const IGNORED: [&str; 12] = [
     // jpeg-decoder failed to decode this file, I don't know how to use zune-jpeg
     // to decode this file with correct color.
     "issue11052.pdf.link",
+    // the same as issue11052.pdf.link
+    "issue1419.pdf.link",
     // this file contains invalid xref, after file scan trailer point to wrong catalog,
     // If resolve catalog dict by check all dict for `/Catalog` type, can find correct
     // catalog dict. `mupdf` also failed to parse this file, but others are okay.
