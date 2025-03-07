c62b3463228c03856d2a7cfbdb1f1762143d1ac5
forks <redforks@gmail.com>
2025-03-07 17:25:57 +0800

    fix: Correct stream offset in indirect object test

--- nipdf/src/parser/object/tests.rs
+++ nipdf/src/parser/object/tests.rs
@@ -112,7 +112,7 @@ fn test_parse_indirect_object_def() -> Result<(), ParserError> {
         o.1,
         Object::Stream(PdfStream(
             dict,
-            BufPos::new(31, Some(2u32), 0),
+            BufPos::new(31, Some(2u32), 3),
             ObjectId::new(100, 1)
         ))
     );
