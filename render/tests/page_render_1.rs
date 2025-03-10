acdfef6dd9a3f6d37fbfe01ed92882c8e5acf983
forks <redforks@gmail.com>
2025-03-10 16:21:08 +0800

    chore: Update IGNORED pdf test files list

--- render/tests/page_render_1.rs
+++ render/tests/page_render_1.rs
@@ -68,7 +68,7 @@ fn download_file(url: &str, p: impl AsRef<Path>) -> Result<()> {
 
 /// These files are very rare and odd, not to be tested
 #[rustfmt::skip]
-const IGNORED: [&str; 30] = [
+const IGNORED: [&str; 31] = [
     // odd FlateDecode stream, xpdf failed to decode, mupdf no problem
     "bug1050040.pdf",
     // invalid object format, mupdf failed to parse, mupdf says no page
@@ -124,6 +124,8 @@ const IGNORED: [&str; 30] = [
     "issue6364.pdf.link",
     // Possible of jpeg_decoder crate failed to decode jpx stream
     "issue12841_reduced.pdf",
+    // contains invalid dct image, okay in release mode. All other pdf readers can decode the image
+    "issue18042.pdf",
 
     // okay in release mode, invalid ColorSpace name
     "issue6707.pdf.link",
@@ -147,13 +149,12 @@ const IGNORED: [&str; 30] = [
     // pdf.js repo test_manifest.json
     "issue19484_1.pdf",
     "issue19484_2.pdf",
+    // all pdf readers failed to open, even pdf.js
+    "poppler-742-0-fuzzed.pdf",
 
     // overflow panic in font-kit crate, rust check arithmetic overflow in debug mode
     "issue16839.pdf.link",
     "issue17689.pdf.link",
-
-    // contains invalid dct image, okay in release mode. All other pdf readers can decode the image
-    "issue18042.pdf",
 ];
 
 static PASSWORD: phf::Map<&'static str, &'static str> = phf::phf_map! {
