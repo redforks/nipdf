1668dbbfbe0752626fee4862bb5142e486b488f3
forks <redforks@gmail.com>
2025-03-20 16:40:15 +0800

    Touch Toolbox for gpui-reader

--- gpui-reader/src/main.rs
+++ gpui-reader/src/main.rs
@@ -1,67 +1,110 @@
+use std::path::PathBuf;
+
 use gpui::{
-    App, Application, Bounds, Context, SharedString, Window, WindowBounds, WindowOptions, div,
-    prelude::*, px, rgb, size,
+    Application, Context, DefaultColors, Entity, KeyBinding, Menu, MenuItem, MouseButton,
+    ObjectFit, SharedString, TitlebarOptions, Window, WindowOptions, actions, div, img, prelude::*,
 };
 
-struct HelloWorld {
+struct ToolboxButton {
     text: SharedString,
+    on_click: Box<dyn Fn(&mut Context<'_, Self>) + 'static>,
 }
 
-impl Render for HelloWorld {
+impl ToolboxButton {
+    fn new(text: impl Into<SharedString>) -> Self {
+        Self {
+            text: text.into(),
+            on_click: Box::new(|_| ()),
+        }
+    }
+}
+
+impl Render for ToolboxButton {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<'_, Self>) -> impl IntoElement {
+        div().child(self.text.clone()).on_mouse_down(
+            MouseButton::Left,
+            cx.listener(|this, _, _, cx| (this.on_click)(cx)),
+        )
+    }
+}
+
+struct Toolbox {
+    buttons: Vec<Entity<ToolboxButton>>,
+}
+
+impl Toolbox {
+    pub fn new(cx: &mut Context<'_, Self>) -> Self {
+        let open = cx.new(|_| ToolboxButton::new("Open..."));
+        let next = cx.new(|_| ToolboxButton::new("Next"));
+        let prev = cx.new(|_| ToolboxButton::new("Prev"));
+        Self {
+            buttons: vec![open, next, prev],
+        }
+    }
+}
+
+impl Render for Toolbox {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<'_, Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .w_full()
+            .gap_x_1()
+            .children(self.buttons.clone())
+    }
+}
+
+struct MyApp {
+    toolbox: Entity<Toolbox>,
+}
+
+impl MyApp {
+    fn new(cx: &mut Context<'_, Self>) -> Self {
+        let toolbox = cx.new(Toolbox::new);
+        Self { toolbox }
+    }
+}
+
+impl Render for MyApp {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<'_, Self>) -> impl IntoElement {
         div()
             .flex()
             .flex_col()
+            .size_full()
             .gap_3()
-            .bg(rgb(0x505050))
-            .size(px(500.0))
-            .justify_center()
-            .items_center()
-            .shadow_lg()
-            .border_1()
-            .border_color(rgb(0x0000ff))
-            .text_xl()
-            .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
+            .px_2()
+            .text_color(gpui::DefaultColor::Text.color(&DefaultColors::dark()))
+            .bg(gpui::DefaultColor::Background.color(&DefaultColors::dark()))
+            .child(self.toolbox.clone())
             .child(
                 div()
-                    .flex()
-                    .gap_2()
-                    .child(div().size_8().bg(gpui::red()))
-                    .child(div().size_8().bg(gpui::green()))
-                    .child(div().size_8().bg(gpui::blue()))
-                    .child(div().size_8().bg(gpui::yellow()))
-                    .child(div().size_8().bg(gpui::black()))
-                    .child(div().size_8().bg(gpui::white())),
+                    .id("main")
+                    .overflow_scroll()
+                    .flex_grow()
+                    .child(img(PathBuf::from("/tmp/new.png")).object_fit(ObjectFit::None)),
             )
     }
 }
 
+actions!(Self, [Quit]);
+
 fn main() {
-    Application::new().run(|cx: &mut App| {
-        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+    Application::new().run(|cx| {
+        cx.on_action(|_: &Quit, cx| cx.quit());
+        cx.bind_keys([KeyBinding::new("ctrl-q", Quit, None)]);
+        cx.set_menus(vec![Menu {
+            name: "File".into(),
+            items: vec![MenuItem::action("Quit", Quit)],
+        }]);
         cx.open_window(
             WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                titlebar: Some(TitlebarOptions {
+                    title: Some("Nipdf Reader".into()),
+                    appears_transparent: false,
+                    ..Default::default()
+                }),
                 ..Default::default()
             },
-            |_, cx| {
-                cx.new(|_| HelloWorld {
-                    text: "World".into(),
-                })
-            },
-        )
-        .unwrap();
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |_, cx| {
-                cx.new(|_| HelloWorld {
-                    text: "汉字".into(),
-                })
-            },
+            |_, cx| cx.new(MyApp::new),
         )
         .unwrap();
     });
